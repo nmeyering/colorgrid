cbd5ae7e811d70fc7bba0cd8430993e4bee575c6
Niels Meyering <nmeyering@gmail.com>
2011-08-18 02:44:28 +0200

    added some game logic

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-#Description
+###Description
 My interpretation of a fun little puzzle game by 'Flood-It!' by Google/LabPixies.
-Color the board uniformly in as little moves as possible.
 
-#Requirements
+Color the board uniformly in as little moves as possible.
+###Requirements
 * Python 3
 * a VT100-style terminal emulator
-
-#Installation
+###Installation
 None.
+
 Just run: ./main.py
--- main.py
+++ main.py
@@ -2,17 +2,21 @@
 import random
 
 colors = [
-"gray",
-"red",
-"green",
-"yellow",
-"blue",
-"magenta",
-"cyan",
-"white"]
+"gray"
+,"red"
+,"green"
+,"yellow"
+,"blue"
+,"magenta"]
+
+"""
+#the original game only has 6 colors...
+,"cyan"
+,"white"]
+"""
 
 class Board:
-	def __init__(self, size=10, block_char='\u2588'):
+	def __init__(self, size=14, cols=6, block_char='\u2588'):
 		self.size = size
 		self.block_char = block_char
 		self.field = []
@@ -20,10 +24,21 @@ class Board:
 			self.field.append([])
 			for x in range(self.size):
 				self.field[y].append(
-					random.randrange(6))
+					random.randrange(cols))
+
+	def uniform(self):
+		c = self.field[0][0]
+		for line in self.field:
+			for tile in line:
+				if not tile == c:
+					return False
+		return True
 
 	def flood(self, col):
-		old = self.field[0][0],
+		if not col in range(len(colors)):
+			raise AttributeError('Invalid color!')
+
+		old = self.field[0][0]
 		if old == col:
 			return
 		return self._flood(
@@ -35,6 +50,8 @@ class Board:
 	def _flood(self, old_color, color, x, y):
 		self.field[y][x] = color
 		for neighbor in range(4):
+			#left, right, up, down:
+			#should yield (-1,0), (1,0), (0,-1), (0,1)
 			dx = 2 * (neighbor % 2) - 1 if neighbor < 2 else 0
 			dy = 2 * (neighbor % 2) - 1 if neighbor >= 2 else 0
 
@@ -43,7 +60,6 @@ class Board:
 				if self.field[y + dy][x + dx] == old_color:
 					ret = True
 					self._flood(old_color, color, x + dx, y + dy)
-
 		return ret
 
 	def __str__(self):
@@ -58,10 +74,10 @@ class Board:
 			ret += '\n'
 		return ret
 
-def main():
-	board = Board(14, '\u2592')
+def game():
+	board = Board(block_char='\u2592')
 
-	instructions = '\033[{}A'.format(
+	instructions = '\033[s\033[{}A'.format(
 		board.size + 1)
 
 	for c in range(len(colors)):
@@ -70,15 +86,37 @@ def main():
 			idx = c,
 			val = colored(colors[c], c))
 
+	instructions += '\n'
+	instructions += '\033[{indent}Cn: New Game\n'.format(
+			indent = 1 + 2 * board.size)
+	instructions += '\033[{indent}Cq: Quit\n'.format(
+			indent = 1 + 2 * board.size)
+	instructions += '\033[u'
+
 	turnlimit = 25
-	for turn in range(turnlimit):
+	turn = 0
+	while turn < turnlimit:
 		print(board)
-		print('\033[s', end='')
 		print(instructions)
-		print('\033[u', end='')
-		color = int(input('your move ({}): '.format(
-			1 + turn)))
-		board.flood(color)
+		cmd = input('your move ({}): '.format(
+			1 + turn))
+		try:
+			board.flood(int(cmd))
+			turn += 1
+		except AttributeError as e:
+			print(e)
+		except ValueError:
+			if cmd == 'n':
+				return True
+			elif cmd == 'q':
+				return False
+		if board.uniform():
+			print('Congratulations, you won!')
+			return not input('Play again? [Y/n] ').lower() == 'n'
+
+def main():
+	while game():
+		pass
 
 def colored(string, color):
 	#0			1				2				3				4				5				6				7
